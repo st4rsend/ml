97c75f32d96e378d36a0a0314ebb8c793d9625ad
psychotictac@gmail.com <psychotictac@gmail.com>
2021-04-30 04:49:15 +0000

    lemma, files rework, ffn & bert

--- ml-python/cloud_backup.ps1
+++ ml-python/cloud_backup.ps1
@@ -1,2 +1,3 @@
 gcloud.cmd compute scp *ipynb terminus:/var/local/st4rsend/ml-angular/ml-python
 gcloud.cmd compute scp *ps1 terminus:/var/local/st4rsend/ml-angular/ml-python
+gcloud.cmd compute scp *txt terminus:/var/local/st4rsend/ml-angular/ml-python
